868669c0190967ed2a7b2964fe8cbdf201a21e5f
leandro-wrf <leandro.ferreira.wr@gmail.com>
2020-08-09 12:14:45 -0300

    add server

---
+++ server/src/routes.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+
+import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
+
+const routes = Router();
+
+routes.post('/classes', ClassesController.create);
+routes.get('/classes', ClassesController.index);
+
+routes.post('/connections', ConnectionsController.create);
+routes.get('/connections', ConnectionsController.index);
+
+export default routes;
